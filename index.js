d241dd738124a2bdb1fa3da18ffb707de283be1d
Kumao-sennsei <gooday.jukucafe@gmail.com>
2025-12-06 15:57:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const globalState = {};
 
 const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 const client = new line.Client(config);
 const app = express();
